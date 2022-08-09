ffd33046eb2b99763067a5ac08b51bed7ec84ddc
Venkata-NavyaSri <110813773+Venkata-NavyaSri@users.noreply.github.com>
2022-08-09 11:34:28 +0530

    Update Two.java

--- Two.java
+++ Two.java
@@ -1,7 +1,13 @@
 import java.io.*;
+import java.util.*;
 public static void main(String[] args){
 	Class HelloWorld{
+		int a,b,c;
 		System.out.println("Hellooo.....World");
-		System.out.println();
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(c);
 	}
 }
